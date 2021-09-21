cf344d559473883d9d25f45daeb0ada06de37ae1
luciferXarya <89343269+luciferXarya@users.noreply.github.com>
2021-09-21 21:22:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,165 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+
+ReactDOM.render(
+<div>
+<body>
+		<header>
+			<div class="website-header">
+				<img src="https://th.bing.com/th/id/OIP.jDJMlZGoSv94izabNIIBIQHaFa?pid=ImgDet&rs=1"></img>
+				<h1>HIGHLIGHTS</h1>
+				<div class="social-icons">
+					<i class="fab fa-facebook-f"></i>
+					<i class="fab fa-twitter"></i>
+					<i class="fab fa-google-plus-g"></i>
+					<i class="fab fa-instagram"></i>
+					<i class="fas fa-rss"></i>
+				</div>
+			</div>
+			<nav class="navbar navbar-expand-md ">
+ 
+  
+
+ 
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  
+  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="#">HOME</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">POLITICS</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">DONATE</a>
+      </li> 
+		<li class="nav-item">
+        <a class="nav-link" href="#">JOIN US</a>
+      </li>
+      
+      
+      <li class="nav-item">
+        <a class="nav-link" href="#">VIDEOS</a>
+      </li> 
+    </ul>
+  </div> 
+			</nav>
+			<div class="breaking-news-section">
+				<span id="btext">Breaking News</span>
+				<marquee direction="left" onmouseover="this.stop()" onmouseout="this.start()">
+					<a href="#" class="breaking-news">
+						<p class="bntime">11 Jan 2019</p>
+						Puri Sankaracharya lorem ipsum</a>
+					<a href="#" class="breaking-news"><p class="bntime">11 Jan 2019</p>Puri Sankaracharya lorem ipsuy</a>
+					<a href="#" class="breaking-news"><p class="bntime">11 Jan 2019</p>Snowstorm buries Pacific Northwest, with more on the way</a>
+				</marquee>
+			</div>
+		</header>
+		<main>
+			<article id="popular-news">
+				<div id="featured">
+					<center><h2>CAPTURE THE LATEST NEWS FEED</h2></center>
+          
+					
+				</div>
+        </article>
+				
+			<section class="more-news">
+				<div class="news-section">
+					<article class="world">
+						<h2></h2>
+						<img src="https://th.bing.com/th/id/OIP.wLJ5jv0m4A-TrYwTRIJQkAHaE7?pid=ImgDet&rs=1"></img>
+						<h3>Puri Sankaracharya lorem ipsum </h3>
+						<p>This is some dummy copy. You’re not really supposed to read this dummy copy, it is just a place holder for people who need some type to visualize what the actual copy might look like if it were real content. If you want to read, I might suggest a good […]</p>
+					</article>
+					<article class="sport">
+						<h2></h2>
+						<img src="https://th.bing.com/th/id/OIP.wLJ5jv0m4A-TrYwTRIJQkAHaE7?pid=ImgDet&rs=1"></img>
+						<h3>Puri Sankaracharya lorem ipsum </h3>
+						<p>This is some dummy copy. You’re not really supposed to read this dummy copy, it is just a place holder for people who need some type to visualize what the actual copy might look like if it were real content. If you want to read, I might suggest a good […]</p>
+					</article>
+				</div>
+				
+				<aside>
+					<h4>UPCOMING EVENTS</h4>
+					<div class="recent-news">
+						<img src="https://th.bing.com/th/id/OIP.wLJ5jv0m4A-TrYwTRIJQkAHaE7?pid=ImgDet&rs=1"></img>
+						<p>Puri Sankaracharya lorem ipsum </p>
+					</div>
+					<div class="recent-news">
+						<img src="https://th.bing.com/th/id/OIP.wLJ5jv0m4A-TrYwTRIJQkAHaE7?pid=ImgDet&rs=1"></img>
+						<p>Puri Sankaracharya lorem ipsum t</p>
+					</div>
+					<div class="recent-news">
+						<img src="https://th.bing.com/th/id/OIP.wLJ5jv0m4A-TrYwTRIJQkAHaE7?pid=ImgDet&rs=1"></img>
+						<p>Puri Sankaracharya lorem ipsum </p>
+					</div>
+					<div class="recent-news">
+						<img src="https://th.bing.com/th/id/OIP.wLJ5jv0m4A-TrYwTRIJQkAHaE7?pid=ImgDet&rs=1"></img>
+						<p>Puri Sankaracharya lorem ipsum </p>
+					</div>
+					<div class="recent-news">
+						<img src="https://th.bing.com/th/id/OIP.wLJ5jv0m4A-TrYwTRIJQkAHaE7?pid=ImgDet&rs=1"></img>
+						<p>Puri Sankaracharya lorem ipsum </p>
+					</div>
+				</aside>
+			</section>
+		</main>
+		<footer>
+			<div class="social-links">
+				<section class="about">
+					<h2>About</h2>
+					<p>Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Curabitur blandit tempus porttitor. Etiam porta sem malesuada magna mollis euismod.</p>
+					<div class="social-icons-footer">
+						<i class="fab fa-facebook"></i>
+						<i class="fab fa-twitter-square"></i>
+						<i class="fab fa-google-plus-square"></i>
+						<i class="fab fa-instagram"></i>
+						<i class="fas fa-rss-square"></i>
+						<i class="fab fa-youtube-square"></i>
+						<i class="fas fa-rss-square"></i>
+						<i class="fab fa-pinterest-square"></i>
+						<i class="fab fa-linkedin"></i>
+					</div>
+				</section>
+				<section class="Instagram">
+					<h2>Instagram</h2>
+					<img src="images/aside2.jpg"></img>
+					<img src="images/aside3.jpg"></img>
+					<img src="images/aside4.jpg"></img>
+					<img src="images/aside5.jpg"></img>
+					<img src="images/aside2.jpg"></img>
+					<img src="images/aside3.jpg"></img>
+					<img src="images/aside4.jpg"></img>
+					<img src="images/aside5.jpg"></img>
+					<img src="images/aside2.jpg"></img>
+					<button type="button" class="footer-buttons">View on Instagram</button>
+				</section>
+				<section class="Subscribe">
+					<h2>Any other information yaha par display hogi</h2>
+					<p>lorem ipsum dolor sit amet, consectetur adipiscing</p>
+					<input type="email" placeholder="Email Address"></input>
+					<button type="button" class="footer-buttons">Subscribe</button>
+				</section>
+			</div>
+			
+			<div class="copyright">Copyright © 2019</div>
+		</footer>
+		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+	</body>
+ </div>
+,
+  document.getElementById('root')
+);
+
+reportWebVitals();
